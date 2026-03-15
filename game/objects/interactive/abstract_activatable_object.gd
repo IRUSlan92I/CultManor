b5b2c7691b1f3da8ced9479255d52d4a24d8c7e9
Ruslan Ignatov <ruslanlena30@gmail.com>
2026-03-15 22:59:24 +0300

    Added AbstractActivatableObject

---
+++ game/objects/interactive/abstract_activatable_object.gd
@@ -0,0 +1,23 @@
+class_name AbstractActivatableObject
+extends Node2D
+
+
+@onready var hint_sprite : Sprite2D = $HintSprite
+
+
+func activate() -> void:
+	pass
+
+
+func _ready() -> void:
+	hint_sprite.hide()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		hint_sprite.show()
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player:
+		hint_sprite.hide()
---
+++ game/objects/interactive/lever.gd
@@ -0,0 +1,5 @@
+extends AbstractActivatableObject
+
+
+func activate() -> void:
+	pass
