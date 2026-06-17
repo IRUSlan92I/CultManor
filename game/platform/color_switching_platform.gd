8812915d3e5b4872cfc0a74f92eb44cecec2bf23
Ruslan Ignatov <ruslanlena30@gmail.com>
2026-06-17 20:54:53 +0300

    Added icon and animation to ColorSwitchingPlatform

--- game/platform/color_switching_platform.gd
+++ game/platform/color_switching_platform.gd
@@ -3,5 +3,13 @@ class_name ColorSwitchingPlatform
 extends AbstractColorPlatform
 
 
+const SWITCH_TIME = 0.2
+
+
+func _ready() -> void:
+	super._ready()
+	icon_sprite.play(ANIMATION_COLOR_SWITCHING)
+
+
 func toggle() -> void:
-	collision_switcher.switch_color()
+	collision_switcher.switch_color(SWITCH_TIME)
